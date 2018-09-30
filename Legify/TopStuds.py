2e0cad296bb76a662080b81d38aaa1fc6bc6d27f
vectronic <hello.vectronic@gmail.com>
2018-09-30 22:46:44 +0100

    refactor: clearer method names

--- Legify/TopStuds.py
+++ Legify/TopStuds.py
@@ -21,8 +21,8 @@ class TopStudsRenderer(object):
         self.depth_count = top_studs_depth_count
 
     @staticmethod
-    def _add_stud_outer_pad_sketch(geometries, constraints, width_offset, depth_offset, style):
-        Console.PrintMessage("_add_stud_outer_pad_sketch({0},{1},{2})\n".format(width_offset, depth_offset, style))
+    def _add_top_stud_outer_pad_sketch(geometries, constraints, width_offset, depth_offset, style):
+        Console.PrintMessage("_add_top_stud_outer_pad_sketch({0},{1},{2})\n".format(width_offset, depth_offset, style))
 
         segment_count = len(geometries)
 
@@ -43,8 +43,8 @@ class TopStudsRenderer(object):
                                                    segment_count + 1, VERTEX_CENTRE_INDEX, depth_offset))
 
     @staticmethod
-    def _add_stud_inside_pocket_sketch(geometries, constraints, width_offset, depth_offset):
-        Console.PrintMessage("_add_stud_inside_pocket_sketch({0},{1})\n".format(width_offset, depth_offset))
+    def _add_top_stud_inside_pocket_sketch(geometries, constraints, width_offset, depth_offset):
+        Console.PrintMessage("_add_top_stud_inside_pocket_sketch({0},{1})\n".format(width_offset, depth_offset))
 
         segment_count = len(geometries)
 
@@ -55,9 +55,9 @@ class TopStudsRenderer(object):
         constraints.append(Sketcher.Constraint("DistanceY", GEOMETRY_ORIGIN_INDEX, VERTEX_START_INDEX, segment_count,
                                                VERTEX_CENTRE_INDEX, depth_offset))
 
-    def _render_studs_outside(self, initial_width_offset, initial_depth_offset, style):
-        Console.PrintMessage("render_studs_outside({0},{1},{2})\n".format(initial_width_offset, initial_depth_offset,
-                                                                          style))
+    def _render_top_studs_outside(self, initial_width_offset, initial_depth_offset, style):
+        Console.PrintMessage("render_top_studs_outside({0},{1},{2})\n".format(
+            initial_width_offset, initial_depth_offset, style))
 
         top_studs_outside_pad_sketch = self.brick.newObject("Sketcher::SketchObject", "top_studs_outside_pad_sketch")
         top_studs_outside_pad_sketch.Support = (self.doc.top_datum_plane, '')
@@ -70,10 +70,10 @@ class TopStudsRenderer(object):
 
         for i in range(0, self.width_count):
             for j in range(0, self.depth_count):
-                self._add_stud_outer_pad_sketch(geometries, constraints,
-                                                initial_width_offset + i * DIMS_STUD_WIDTH_INNER,
-                                                initial_depth_offset + j * DIMS_STUD_WIDTH_INNER,
-                                                style)
+                self._add_top_stud_outer_pad_sketch(geometries, constraints,
+                                                    initial_width_offset + i * DIMS_STUD_WIDTH_INNER,
+                                                    initial_depth_offset + j * DIMS_STUD_WIDTH_INNER,
+                                                    style)
 
         top_studs_outside_pad_sketch.addGeometry(geometries, False)
         top_studs_outside_pad_sketch.addConstraint(constraints)
@@ -104,8 +104,8 @@ class TopStudsRenderer(object):
 
         self.doc.recompute()
 
-    def _render_studs_inside(self, initial_width_offset, initial_depth_offset):
-        Console.PrintMessage("render_studs_inside({0},{1})\n".format(initial_width_offset, initial_depth_offset))
+    def _render_top_studs_inside(self, initial_width_offset, initial_depth_offset):
+        Console.PrintMessage("render_top_studs_inside({0},{1})\n".format(initial_width_offset, initial_depth_offset))
 
         top_studs_inside_pocket_sketch = self.brick\
             .newObject("Sketcher::SketchObject", "top_studs_inside_pocket_sketch")
@@ -117,9 +117,9 @@ class TopStudsRenderer(object):
 
         for i in range(0, self.width_count):
             for j in range(0, self.depth_count):
-                self._add_stud_inside_pocket_sketch(geometries, constraints,
-                                                    initial_width_offset + i * DIMS_STUD_WIDTH_INNER,
-                                                    initial_depth_offset + j * DIMS_STUD_WIDTH_INNER)
+                self._add_top_stud_inside_pocket_sketch(geometries, constraints,
+                                                        initial_width_offset + i * DIMS_STUD_WIDTH_INNER,
+                                                        initial_depth_offset + j * DIMS_STUD_WIDTH_INNER)
 
         top_studs_inside_pocket_sketch.addGeometry(geometries, False)
         top_studs_inside_pocket_sketch.addConstraint(constraints)
@@ -128,9 +128,6 @@ class TopStudsRenderer(object):
         top_studs_inside_pocket = self.brick.newObject("PartDesign::Pocket", "top_studs_inside_pocket")
         top_studs_inside_pocket.Profile = top_studs_inside_pocket_sketch
         top_studs_inside_pocket.Reversed = True
-        # Need to specify Dimension instead of UpToFace because of the following issue:
-        # https://freecadweb.org/tracker/view.php?id=3177
-        # https://forum.freecadweb.org/viewtopic.php?f=8&t=24238
         top_studs_inside_pocket.Length = DIMS_TOP_THICKNESS + DIMS_STUD_INSIDE_HOLE_TOP_OFFSET
 
         top_studs_inside_pocket_sketch.ViewObject.Visibility = False
@@ -143,8 +140,8 @@ class TopStudsRenderer(object):
         initial_width_offset = (self.width - self.width_count) * DIMS_STUD_WIDTH_INNER / 2
         initial_depth_offset = (self.depth - self.depth_count) * DIMS_STUD_WIDTH_INNER / 2
 
-        self._render_studs_outside(initial_width_offset, initial_depth_offset, self.style)
+        self._render_top_studs_outside(initial_width_offset, initial_depth_offset, self.style)
 
         # Only render inner pocket if closed studs AND studs are not offset
         if self.style == TopStudStyle.CLOSED and initial_width_offset == 0 and initial_depth_offset == 0:
-            self._render_studs_inside(initial_width_offset, initial_depth_offset)
+            self._render_top_studs_inside(initial_width_offset, initial_depth_offset)
